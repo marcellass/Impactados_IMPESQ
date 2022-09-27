3cd1151f86260f199ac52db252fc32c7197792d9
Marcella <marcella.ssanchesbe@gmail.com>
2022-09-27 18:06:59 -0300

    Correção retorno dados da pesquisa

--- Cadastro.py
+++ Cadastro.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'impactados'
 app.config['MYSQL_DATABASE_DB'] = 'pesquisa'
-app.config['MYSQL_DATABASE_HOST'] = ''
+app.config['MYSQL_DATABASE_HOST'] = 'db'
 mysql.init_app(app)
 
 @app.route('/')
@@ -23,13 +23,13 @@ def cadastrar():
     objetoPesquisa = request.form['objetoPesquisa']
     tipoObjeto = request.form['tipoObjeto']
     horapesquisa = str(request.form['horapesquisa'])
+    genero = request.form['genero']
     faixaEtaria = request.form['faixaEtaria']
     classeEconomica = request.form['classeEconomica']
-
-    if nomeEmpresa and cnpj and objetoPesquisa and tipoObjeto and horapesquisa and faixaEtaria and classeEconomica:
+    if nomeEmpresa and cnpj and objetoPesquisa and tipoObjeto and horapesquisa and genero and faixaEtaria and classeEconomica:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute('insert into tbl_objeto_pesquisa (empresa_nome, cnpj, objeto_pesquisa, tipo_objeto, hora_pesquisa, faixa_etaria, faixa_etaria) VALUES (%s, %s, %s, %s, %s, %s, %s)', (nomeEmpresa, cnpj, objetoPesquisa, tipoObjeto, horapesquisa, faixaEtaria, classeEconomica))
+        cursor.execute('insert into tbl_objeto_pesquisa (empresa_nome, cnpj, objeto_pesquisa, tipo_objeto, hora_pesquisa, genero, faixa_etaria, classe_economica) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (nomeEmpresa, cnpj, objetoPesquisa, tipoObjeto, horapesquisa, genero, faixaEtaria, classeEconomica))
         conn.commit()  
 
     return render_template('cadastro_pesquisa.html')
@@ -38,12 +38,12 @@ def cadastrar():
 def listar():
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute('select empresa_nome, cnpj, objeto_pesquisa, tipo_objeto, hora_pesquisa, faixa_etaria, faixa_etaria from tbl_objeto_pesquisa')
+    cursor.execute('select empresa_nome, cnpj, objeto_pesquisa, tipo_objeto, hora_pesquisa, genero, faixa_etaria, classe_economica from tbl_objeto_pesquisa')
     data = cursor.fetchall()
     conn.commit()
     return render_template('listar_pesquisa.html', datas=data)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8885))
+    port = int(os.environ.get("PORT", 8888))
     app.run(host='0.0.0.0', port=port)
 
