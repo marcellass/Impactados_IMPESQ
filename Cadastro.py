31ef2a405a47bd06eeae3ffce7be912f16607f09
Marcella <marcella.ssanchesbe@gmail.com>
2022-09-29 19:13:15 -0300

    teste

--- Cadastro.py
+++ Cadastro.py
@@ -25,8 +25,7 @@ def cadastrar():
     horapesquisa = str(request.form['horapesquisa'])
     genero = request.form['genero']
     faixaEtaria = request.form['faixaEtaria']
-    if request.method == "POST":
-        classeEconomica = request.form.getlist("classeEconomica")
+    classeEconomica = request.form['classeEconomica']
     if nomeEmpresa and cnpj and objetoPesquisa and tipoObjeto and horapesquisa and genero and faixaEtaria and classeEconomica:
         conn = mysql.connect()
         cursor = conn.cursor()
